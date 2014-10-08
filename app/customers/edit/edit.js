68371e654aa2dceac34ba5d7a0f491308fc1c83d
Will <will.soto9@gmail.com>
2014-10-08 08:28:27 -0400

    Add save

--- app/customers/edit/edit.js
+++ app/customers/edit/edit.js
@@ -4,7 +4,8 @@
     var moduleName = 'customers.edit',
 
         angularDependencies = [
-            'ui.router'
+            'ui.router',
+            'customers.resources'
         ];
 
     define([
@@ -12,7 +13,8 @@
         'angular',
         'lodash',
         '../models/customer',
-        'ui.router'
+        'ui.router',
+        '../resources'
     ], function(require, angular, _, Customer) {
 
         var module = angular.module(moduleName, angularDependencies);
@@ -26,21 +28,45 @@
                     url: '/edit/:customerId',
                     templateUrl: require.toUrl('./_edit.html'),
                     resolve: {
-                        customer: ['$http', '$stateParams', function($http, $stateParams) {
-                            return $http.get(require.toUrl('../customers.json')).then(function(response) {
-                                return new Customer(_.find(response.data, {
-                                    customer_id: parseInt($stateParams.customerId)
-                                }));
-                            });
-                        }]
+                        customer: ['$http', '$stateParams', 'CustomersResource',
+                            function($http, $stateParams, CustomersResource) {
+                                var id = $stateParams.customerId;
+
+                                if (_.isString(id) && id === 'new') {
+                                    return CustomersResource.newCustomer();
+                                }
+
+                                return $http.get(require.toUrl('../customers.json')).then(function(response) {
+                                    return new Customer(_.indexBy(response.data, 'customer_id')[id]);
+                                });
+                            }
+                        ]
                     }
                 });
             }
         ]);
 
-        module.controller('CustomersEditCtrl', ['customer',
-            function(customer) {
-                this.customer = customer || {};
+        module.controller('CustomersEditCtrl', ['CustomersResource', 'customer',
+            function(CustomersResource, customer) {
+                this.customer = customer;
+
+                this.save = function() {
+                    var save = null;
+
+                    if (this.customer.customer_id !== undefined) {
+                        save = CustomersResource.update(this.customer.customer_id, this.customer);
+                    } else {
+                        save = CustomersResource.create(this.customer);
+                    }
+
+                    save.then(
+                        function success(response) {
+                            console.log(response);
+                        }, function error(response) {
+                            console.log(response);
+                        }
+                    );
+                };
             }
         ]);
 
